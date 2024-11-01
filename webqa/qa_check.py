314d38d70da9e2ba224b2f76f3f380c330b28e2f
RaoNikitha <nikitharaobs@gmail.com>
2024-11-01 16:58:37 +0000

    single script for QA

--- webqa/qa_check.py
+++ webqa/qa_check.py
@@ -7,15 +7,15 @@ import sys
 sys.path.append("..")
 from eval.eval_utils import *
 import gc
+import argparse
 
 if __name__ == "__main__":
-    import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_path", type=str, default="Qwen/Qwen2-VL-7B-Instruct-GPTQ-Int4")
     parser.add_argument("--version", type=str, default="2")
     parser.add_argument("--perturbation_path", type=str, required=True)
-    parser.add_argument("--output_file", type=str, default="qa_check_counterfactuals")
     parser.add_argument("--eval_data_path", type=str, required=True)
+    parser.add_argument("--counterfactual", type=bool, default=True)
     args = parser.parse_args()
     # model_path = "Qwen/Qwen2-VL-72B-Instruct-AWQ" #
     # model_path = "Qwen/Qwen2-VL-7B-Instruct" # 
@@ -23,13 +23,15 @@ if __name__ == "__main__":
     model_path = args.model_path
     version = args.version
     perturbation_path = args.perturbation_path
-    output_file = args.output_file
     eval_data = json.load(open(args.eval_data_path, "r"))
+    use_counterfactual_question = args.counterfactual
 
     model, processor = get_model_processor(model_path)
     conversational_prompt = not 'Phi' in model_path
-    output_file = __file__.split("/")[-1].replace(".py", "")
-    with open(f"data/{output_file}_v{version}.csv", "w") as f:
+    output_str = "counterfactual" if use_counterfactual_question else "perturbation"
+    output_file = f"data/qa_checkk_{output_str}_v{version}.csv"
+    
+    with open(output_file, "w") as f:
         f.write("model,file,qa_check\n")
 
     for file in tqdm(os.listdir(perturbation_path)):
@@ -39,7 +41,11 @@ if __name__ == "__main__":
         key = file.split("_")[1].split(".")[0]
         example = eval_data[key]
         counterfactual_image_file = f"{perturbation_path}/{file}"
-        question = f"Q: is the {example['Q_obj']} in both the original image and the perturbed image?"
+        if use_counterfactual_question:
+            question = f"Q: is the {example['Q_obj']} in both the original image and the perturbed image?"
+        else:
+            question = f"Q: is the {example['Qcate']} of the {example['Q_obj']} the same in both images?"
+
         messages = get_qa_check_prompt(question, conversational_prompt)
         images = None
         try:
@@ -54,6 +60,6 @@ if __name__ == "__main__":
             gc.collect()
             continue
         qa_check = qa_check.split('\n')[-1]
-        with open(f"data/{output_file}_v{version}.csv", "a") as f:
+        with open(output_file, "a") as f:
             f.write(f"{model_path},{file},\"{qa_check}\"\n")
 
